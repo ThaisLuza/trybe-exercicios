9aa5dde5256361c0921b691ca70e567937914550
Thais Luza <thaisluza21@hotmail.com>
2021-10-11 15:02:26 -0300

    Maior de dois números

--- fundamentos-do-desenvolvimento-web/bloco04-introduçao-javascript-e-logica-de-programacao/dia1-javascript-primeiros-passos/script.js
+++ fundamentos-do-desenvolvimento-web/bloco04-introduçao-javascript-e-logica-de-programacao/dia1-javascript-primeiros-passos/script.js
@@ -1,16 +1,26 @@
-let a = 10;
-let b = 5;
-let resultadoAdicao = (a + b);
-console.log(resultadoAdicao);
+// let a = 10;
+// let b = 5;
+// let resultadoAdicao = (a + b);
+// console.log(resultadoAdicao);
 
-let resultadoSubtracao = (a - b);
-console.log(resultadoSubtracao);
+// let resultadoSubtracao = (a - b);
+// console.log(resultadoSubtracao);
 
-let resultadoMultiplicacao = (a * b);
-console.log(resultadoMultiplicacao); 
+// let resultadoMultiplicacao = (a * b);
+// console.log(resultadoMultiplicacao); 
 
-let resultadoDivisao = (a / b);
-console.log(resultadoDivisao);
+// let resultadoDivisao = (a / b);
+// console.log(resultadoDivisao);
 
-let resultadoModulo = (a % b);
-console.log(resultadoModulo);
+// let resultadoModulo = (a % b);
+// console.log(resultadoModulo);
+
+
+const a = 5;
+const b = 10;
+if(a>b){
+  console.log(a);
+}
+else {
+  console.log(b);
+}
