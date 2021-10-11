66f3f8211f6f6972349ae2416bd837517525753a
Thais Luza <thaisluza21@hotmail.com>
2021-10-11 14:47:47 -0300

    Operações aritméticas

---
+++ fundamentos-do-desenvolvimento-web/bloco04-introduçao-javascript-e-logica-de-programacao/dia1-javascript-primeiros-passos/script.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 5;
+let resultadoAdicao = (a + b);
+console.log(resultadoAdicao);
+
+let resultadoSubtracao = (a - b);
+console.log(resultadoSubtracao);
+
+let resultadoMultiplicacao = (a * b);
+console.log(resultadoMultiplicacao); 
+
+let resultadoDivisao = (a / b);
+console.log(resultadoDivisao);
+
+let resultadoModulo = (a % b);
+console.log(resultadoModulo);
